db6e65996ac67be66781874048ac1de7b398bafb
Alicia MARCHAND <amarchan@e3r8p13.clusters.42paris.fr>
2022-12-16 15:26:10 +0100

    first draft for ft:stack

---
+++ stack/stack.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+# include <iostream>
+# include <vector>
+
+namespace ft 
+{
+	template <class T, class Container = std::vector<T> >
+	class stack 
+	{
+		public:
+			typedef	typename	Container::value_type	value_type;
+			typedef	typename	Container::size_type	size_type;
+			typedef				Container				container_type;
+
+		protected:
+			Container c;
+
+		public:
+			explicit stack(const Container & = Container()) 
+			: c(cont) 
+			{}
+
+			bool empty() const 
+			{
+				return c.empty();
+			}
+
+			size_type size() const 
+			{
+				return c.size();
+			}
+
+			value_type &	top() 
+			{
+				return c.back();
+			}
+
+			const value_type& top() const 
+			{
+				return c.back();
+			}
+
+			void push(const value_type & x) 
+			{
+				c.push_back(x);
+			}
+			
+			void pop() 
+			{
+				c.pop_back();
+			}
+
+			friend bool operator<(const stack<T, Container>& x, const stack<T, Container>& y)
+			{ 
+				return (x.c < y.c);
+			}
+
+			friend bool operator>(const stack<T, Container>& x, const stack<T, Container>& y)
+			{ 
+				return (x.c > y.c); 
+			}
+
+			friend bool operator<=(const stack<T, Container>& x, const stack<T, Container>& y)
+			{ 
+				return (x.c <= y.c);
+			}
+
+			friend bool operator>=(const stack<T, Container>& x, const stack<T, Container>& y)
+			{ 
+				return (x.c >= y.c);
+			}
+
+			friend bool operator==(const stack<T, Container>& x, const stack<T, Container>& y)
+			{ 
+				return (x.c == y.c);
+			}
+
+			friend bool operator!=(const stack<T, Container>& x, const stack<T, Container>& y)
+			{ 
+				return (x.c != y.c);
+			}
+	};
+}
