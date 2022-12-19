af62a8a9518b5070099da566cffe3fdaa5f010f5
Alicia MARCHAND <amarchan@e3r8p13.clusters.42paris.fr>
2022-12-19 11:24:28 +0100

    finish testing comparison operators

--- srcs/stack/main.cpp
+++ srcs/stack/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 			std::stack<int>	std_stack;
 			ft::stack<int>	ft_stack;
 	
-			std::cout << DODGERBLUE1 << "TEST #1" << std::endl << std::endl << RESET;
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
 	
 			std::cout << GREEN3 << "Testing .empty() --> " << std::endl << RESET;
 			std::cout << OLIVE << "std_stack returns : ";
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
 		{
 			std::stack<int>	std_stack;
 			ft::stack<int>	ft_stack;
-			std::cout << DODGERBLUE1 << "TEST #2" << std::endl << std::endl << RESET;
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
 
 			std::cout << GREEN3 << "Testing .size() --> " << std::endl << RESET;
 			std::cout << OLIVE << "std_stack returns : ";
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
 		{
 			std::stack<int>	std_stack;
 			ft::stack<int>	ft_stack;
-			std::cout << DODGERBLUE1 << "TEST #3" << std::endl << std::endl << RESET;
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
 	
 			std::cout << GREEN3 << "Testing .push() and .top() --> " << std::endl << RESET;
 			std::cout << OLIVE << "std_stack returns : ";
@@ -65,7 +65,7 @@ int main(int argc, char **argv)
 		}
 		if (test_number == 4)
 		{
-			std::cout << DODGERBLUE1 << "TEST #4" << std::endl << std::endl << RESET;
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
 
 			std::cout << "Creating two containers stack with 'vector' as underlying container."
 					<< " The vector's name is 'gandalf' and it contains 2 elements of type "
@@ -89,6 +89,68 @@ int main(int argc, char **argv)
 			ft_stack.pop();
 			std::cout << ORANGE1 << "ft_stack returns : ";
 			std::cout << ft_stack.top() << std::endl << RESET;
+			std::cout << std::endl << DODGERBLUE1
+				<< "#########################################################"
+				<< std::endl << RESET;
+		}
+			if (test_number == 5)
+		{
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
+
+			std::cout << "Creating two containers stack with 'vector' as underlying container."
+					<< " The vector's name is 'gandalf' and it contains 2 elements of type "
+					<< "'int' and of value '4'..." << std::endl;
+			std::vector<int> gandalf(2, 4);
+			std::stack<int, std::vector<int> >	std_frodo(gandalf);
+			ft::stack<int>	ft_frodo(gandalf);
+			std::cout << std::endl;
+
+			std::cout << "Creating 2 more vectors std_samwise and ft_samwise..."
+				<< std::endl;
+			std::stack<int, std::vector<int> >	std_samwise(gandalf);
+			ft::stack<int>	ft_samwise(gandalf);
+
+			std::cout << "Pushing 1 element of value 1 in std_stack." << std::endl;
+			std::cout << OLIVE << "std_stack returns : ";
+			std_frodo.push(1);
+			std::cout << std_frodo.top() << std::endl << RESET;
+
+			std::cout << "Pushing 1 element of value 1 in std_stack." << std::endl;
+			ft_frodo.push(1);
+			std::cout << ORANGE1 << "ft_stack returns : ";
+			std::cout << ft_frodo.top() << std::endl << RESET;
+			std::cout << std::endl;
+			
+			std::cout << GREEN3 << "Testing operator < --> " << std::endl << RESET;
+			std::cout << OLIVE << "std_frodo < std_samwise : " << (std_frodo < std_samwise) << std::endl << RESET;
+			std::cout << ORANGE1 << "ft_frodo < ft_samwise : " << (ft_frodo < ft_samwise);
+			std::cout << std::endl << std::endl;
+			
+			std::cout << GREEN3 << "Testing operator > --> " << std::endl << RESET;
+			std::cout << OLIVE << "std_frodo > std_samwise : " << (std_frodo > std_samwise) << std::endl << RESET;
+			std::cout << ORANGE1 << "ft_frodo > ft_samwise : " << (ft_frodo > ft_samwise);
+			std::cout << std::endl << std::endl;
+
+			std::cout << GREEN3 << "Testing operator <= --> " << std::endl << RESET;
+			std::cout << OLIVE << "std_frodo <= std_samwise : " << (std_frodo <= std_samwise) << std::endl << RESET;
+			std::cout << ORANGE1 << "ft_frodo <= ft_samwise : " << (ft_frodo <= ft_samwise);
+			std::cout << std::endl << std::endl;
+
+			std::cout << GREEN3 << "Testing operator >= --> " << std::endl << RESET;
+			std::cout << OLIVE << "std_frodo >= std_samwise : " << (std_frodo >= std_samwise) << std::endl << RESET;
+			std::cout << ORANGE1 << "ft_frodo >= ft_samwise : " << (ft_frodo >= ft_samwise);
+			std::cout << std::endl << std::endl;
+
+			std::cout << GREEN3 << "Testing operator == --> " << std::endl << RESET;
+			std::cout << OLIVE << "std_frodo == std_samwise : " << (std_frodo == std_samwise) << std::endl << RESET;
+			std::cout << ORANGE1 << "ft_frodo == ft_samwise : " << (ft_frodo == ft_samwise);
+			std::cout << std::endl << std::endl;
+
+			std::cout << GREEN3 << "Testing operator != --> " << std::endl << RESET;
+			std::cout << OLIVE << "std_frodo != std_samwise : " << (std_frodo != std_samwise) << std::endl << RESET;
+			std::cout << ORANGE1 << "ft_frodo != ft_samwise : " << (ft_frodo != ft_samwise);
+			std::cout << std::endl << std::endl;
+
 			std::cout << std::endl << DODGERBLUE1
 				<< "#########################################################"
 				<< std::endl << RESET;
