4dc08152e9575ca804d9120d0f4d20ae4ae65abd
Alicia MARCHAND <amarchan@e3r8p13.clusters.42paris.fr>
2022-12-19 17:44:58 +0100

    testing default constructor, param1 constructor, param2 constructor, and accessor []

--- srcs/vector/main.cpp
+++ srcs/vector/main.cpp
@@ -1,5 +1,5 @@
 #include "vector.hpp"
-#include "../../cool_tools/colors/colors.hpp"
+
 #include <vector>
 #include <stdlib.h>
 
@@ -16,9 +16,68 @@ int main(int argc, char **argv)
 		if (test_number == 1)
 		{
 			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
+
+			std::vector<int>	std_vector;
+			ft::vector<int>		ft_vector;
+			std::cout << OLIVE << sizeof(std_vector) << RESET << std::endl;
+			std::cout << ORANGE1 << sizeof(ft_vector) << RESET << std::endl;
+
+
 			std::cout << std::endl << DODGERBLUE1
-				<< "#########################################################"
-				<< std::endl << RESET;
+			<< "#########################################################"
+			<< std::endl << RESET;
+		}
+
+		if (test_number == 2)
+		{
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
+
+			std::vector<char> std_vector_0 (4);
+			std::cout << "Printing content of std_vector_0 :" << std::endl;
+			std::cout << "- START -" << std::endl;
+			for (size_t i = 0; i < std_vector_0.size(); ++i)
+    			std::cout << i << " : " << std_vector_0[i] << std::endl;
+			std::cout << "- END -" << std::endl;
+
+			ft::vector<int> ft_vector_0 (4);
+			std::cout << "Printing content of ft_vector_0 :" << std::endl;
+			std::cout << "- START -" << std::endl;
+			for (size_t i = 0; i < ft_vector_0.size(); ++i)
+    			std::cout << i << " : " << ft_vector_0[i] << std::endl;
+			std::cout << "- END -" << std::endl;
+
+			std::vector<int> std_vector (4, 6);
+			ft::vector<int> ft_vector (4, 6);
+			
+			std::cout << "Printing content of std_vector :" << std::endl;
+			std::cout << "- START -" << std::endl;
+			for (size_t i = 0; i < std_vector.size(); ++i)
+    			std::cout << i << " : " << std_vector[i] << std::endl;
+			std::cout << "- END -" << std::endl;
+			
+			std::cout << "Printing content of ft_vector :" << std::endl;
+			std::cout << "- START -" << std::endl;
+			for (size_t i = 0; i < ft_vector.size(); ++i)
+    			std::cout << i << " : " << ft_vector[i] << std::endl;
+			std::cout << "- END -" << std::endl;
+			std::cout << std::endl << DODGERBLUE1
+			<< "#########################################################"
+			<< std::endl << RESET;
+		}
+
+		if (test_number == 3)
+		{
+			std::cout << DODGERBLUE1 << "TEST #" << test_number << std::endl << std::endl << RESET;
+
+			int myints[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			std::vector<int> std_vector (myints, myints+10);
+			std::cout << "Printing content of std_vector :" << std::endl;
+			for (size_t i = 0; i < std_vector.size(); ++i)
+    		std::cout << std_vector[i] << ' ' << std::endl;
+
+			std::cout << std::endl << DODGERBLUE1
+			<< "#########################################################"
+			<< std::endl << RESET;
 		}
 	}
 	catch(const std::exception& e)
