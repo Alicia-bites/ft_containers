d69ea12c5b558a6c4ea4c4ce6e9edf4489109019
Alicia MARCHAND <amarchan@e3r8p13.clusters.42paris.fr>
2022-12-19 12:17:00 +0100

    added comment about using the key word 'typename' instead of 'class'

--- srcs/vector/vector.hpp
+++ srcs/vector/vector.hpp
@@ -2,7 +2,18 @@
 
 #include <iostream>
 
-class Vector
+// Remarque :
+// Dans la definition d'un patron, on utilise le mot 'class'
+// pour indiquer en fait un type quelconque, classe ou non.
+// (comme ca --> 'template <class T> T' qui veut dire : le
+// type T peut etre n'importe quel type.)
+// Nonobstant, la norme a introduit le mot cle 'typename'
+// pour remplacer le mot 'class' qui fait quand meme plus
+// sens.
+
+
+template <typename T, typename Allocator = std::allocator<T> >
+class vector
 {
 	private:
 		// number of elements in tab
