819c4d85c17addd3846aba5cb5352937ca4286d3
Alicia MARCHAND <amarchan@e1r7p2.clusters.42paris.fr>
2022-12-20 17:03:04 +0100

    designed my own type_traits header. param3 constructor done

---
+++ cool_tools/type_traits.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+// If B is true, std::enable_if has a public member typedef type.
+// equal to T; otherwise, there is no member typedef.
+
+namespace ft
+{
+    template <typename T, T v>
+        struct integral_constant
+        {
+			static const T					value = v;
+			typedef T						value_type;
+			typedef integral_constant<T,v>	type;
+			operator T() const				
+			{ 
+				return v; 
+			}
+        };
+
+    typedef integral_constant<bool,true>	true_type;
+    typedef integral_constant<bool,false>	false_type;
+    
+    template<typename T>
+	struct is_integral : public false_type
+	{};
+    
+    template<>	struct is_integral<unsigned char>			: public true_type {};
+    template<>	struct is_integral<unsigned short int>		: public true_type {};
+    template<>	struct is_integral<unsigned int>	  		: public true_type {};
+    template<>	struct is_integral<unsigned long int>       : public true_type {};
+    template<>	struct is_integral<unsigned long long int>  : public true_type {};
+    template<>	struct is_integral<signed char>	   			: public true_type {};
+    template<>	struct is_integral<short int>	     		: public true_type {};
+    template<>	struct is_integral<int>	 					: public true_type {};
+    template<>	struct is_integral<long int>	      		: public true_type {};
+    template<>	struct is_integral<long long int>	 		: public true_type {};
+    template<>	struct is_integral<char>					: public true_type {};
+    template<>	struct is_integral<wchar_t>					: public true_type {};
+    template<>	struct is_integral<bool>					: public true_type {};
+
+
+    template<bool B, typename T = void>
+        struct enable_if
+		{};
+ 
+    template<typename T>
+        struct enable_if<true, T>
+		{ 
+			typedef T type;
+		};
+}
