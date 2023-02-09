afb59c78d9b5c314d382255612613c4393b37cdb
Alicia <alicia.mrchd@gmail.com>
2023-02-09 19:43:51 +0100

    created a new Node class suitable for red black tree

--- includes/node.hpp
+++ includes/node.hpp
@@ -1,29 +1,119 @@
 #pragma once
 
-enum Color
+#include <iostream>
+
+namespace ft
 {
-	RED,
-	BLACK
-};
-
-// defines a single node in the red-black tree
-template<typename Key, typename Value>
-	struct Node
+	enum Color
 	{
-		Key		key_; // 1st template parameter
-		Value	value_; // 2nd template parameter
-		bool	color_;
-		Node	*left_; // stores a pointer to the child on the left
-		Node	*right_; // stores a pointer to the child on the right
-		Node	*parent_; // stores a pointer to the parent of the node
-
-		// default constructor
-		Node(Key key, Value value)
-		: key_(key)
-		, value_(value)
-		, color_(RED)
-		, left_(NULL)
-		, right_(NULL)
-		, parent_(NULL)
-		{}
+		RED,
+		BLACK
 	};
+
+	template <typename Key, typename Value>
+	class Node
+	{
+		public:
+
+			Key					key;
+			Value				value;
+			Color				color;
+			Node<Key, Value>	*left;
+			Node<Key, Value>	*right;
+			Node<Key, Value>	*parent;
+
+			// default constructor
+			Node()
+			: key(0)
+			, value(0)
+			, color(RED)
+			, left(0)
+			, right(0)
+			, parent(0)
+			{};
+
+			// constructor #1
+			Node(const Key & key, const Value & value)
+			: key(key)
+			, value(value)
+			, color(RED)
+			, left(0)
+			, right(0)
+			, parent(0)
+			{};
+
+			// copy constructor
+			Node(const Node & original)
+			{
+				if (this != &original)
+				{
+					key = original.key;
+					value = original.value;
+					color = original.color;
+					left = original.left;
+					right = original.right;
+					parent = original.parent;
+				}
+			}
+	};
+
+	template<typename Key, typename Value>
+	std::ostream & operator<<(std::ostream & o, const Node<Key, Value> & rhs)
+	{
+		
+		o << "Printing node..." << std::endl;
+		o << DARKORANGE1 << "key = "
+			<< rhs.key
+			<< std::endl
+			<< "value = "
+			<< rhs.value
+			<< std::endl
+			<< "color = "
+			<< rhs.color
+			<< RESET
+			<< std::endl;
+		if (rhs.left)
+		{
+			o << CYAN1 << "left->key = "
+				<< rhs.left->key
+				<< std::endl
+				<< "left->value = "
+				<< rhs.left->key
+				<< std::endl
+
+				<< "color = "
+				<< rhs.color
+				<< RESET
+				<< std::endl;
+		}
+		if (rhs.right)
+		{
+			o << CHARTREUSE5 << "right->key = "
+			<< rhs.right->key
+			<< std::endl
+			<< "right->value = "
+			<< rhs.right->key
+			<< std::endl
+			<< "color = "
+			<< rhs.color
+			<< RESET
+			<< std::endl;
+		}
+		if (rhs.parent)
+		{
+			o << DEEPPINK1 << "right->key = "
+			<< rhs.parent->key
+			<< std::endl
+			<< "right->value = "
+			<< rhs.parent->key
+			<< std::endl
+			<< "color = "
+			<< rhs.color
+			<< RESET
+			<< std::endl;
+		}
+
+		o << "-------------------------------------------------------------------------" << std::endl;
+		return o;
+	}
+}
