ade888d690bdb3bf8121cc877b06524b5b78f556
Alicia MARCHAND <amarchan@e1r12p2.clusters.42paris.fr>
2023-02-23 14:34:55 +0100

    commented useless stuff

--- includes/iterator.hpp
+++ includes/iterator.hpp
@@ -10,11 +10,11 @@
 
 namespace ft
 {
-	struct input_iterator_tag {};
-	struct forward_iterator_tag {};
-	struct bidirectional_iterator_tag	: public input_iterator_tag {};
-	struct random_access_iterator_tag	: public forward_iterator_tag {};
-	struct output_iterator_tag			: public bidirectional_iterator_tag {};
+	// struct input_iterator_tag {};
+	// struct forward_iterator_tag {};
+	// struct bidirectional_iterator_tag	: public input_iterator_tag {};
+	// struct random_access_iterator_tag	: public forward_iterator_tag {};
+	// struct output_iterator_tag			: public bidirectional_iterator_tag {};
 
 	// our base for different iterator types. Stock all the caracteristic of an
 	// iterator for later use.
